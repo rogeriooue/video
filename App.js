f6875981655ff423912cfb3c7530b5fdea00295d
Bowie <rogerio.oue@gmail.com>
2024-03-18 01:27:02 -0300

    changed folder

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ export default function App() {
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
+      const { status } = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === 'granted');
     })();
   }, []);
